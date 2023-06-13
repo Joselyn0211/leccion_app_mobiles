4a0d1cfd3090e62047fb94f704e3ffeb3fa8a539
Joselyn Banchon <100228069+Joselyn0211@users.noreply.github.com>
2023-06-12 19:24:24 -0500

    second commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'leccion_joselyn_banchon',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
